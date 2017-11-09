7167d71b8c5ec41832af81fc40c1a0f2d34caf37
Gerardo Fernandez <gerardojose13@gmail.com>
2017-11-09 01:05:02 -0800

    pushes updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,70 +1,62 @@
 // Hangman game
 
-// 1.- create a pool of words to be used
-// 2.- create a var that will randomize the order of output for arr
-// 3.- create a function that activates the game once a letter is pressed
-// 4.- create a function that grabs the word from the array
-// 5.- create a function that checks if "pressedKey" is present within the chosen word
-
-//Pool of words
+// Pool of words
 var arr = ["beethoven", "mozart", "brahms", "lizst", "chopin", "rachmaninov", "tchaikovski", "mendelson", "bach"];
 
 //Random word generator
-var wordGenerator = arr[Math.floor(Math.random() * arr.length)];
+var numGenerator = Math.floor(Math.random() * arr.length);
+var wordGenerator = arr[numGenerator];
+
+//empty array variable where the "_" will be stored based on the length of the arr
+var underScore = [];
+// var noComasUnderScore = underScore.split().join(" ");
+console.log(wordGenerator);
+
 
 
 //keyboard activation function
 document.onkeyup = function(){
 	var pressedKey = event.key.toLowerCase();
-	var word = wordGenerator;
-
 	console.log(pressedKey);
-	console.log(word);
-	underScore();
-	keyIndex();
 
-	if (keyIndex != -1){
-		// console.log("you guessed correctly");
-	}else if (index == -1){
-		console.log("Please try again");
-	}else {
-		console.log("Make a valid entry");
+
+	if (event.keyCode === 32){
+		generateUnderScore();
 	}
 
-
-	//function to convert the word from the pool into "_ _ _ _ _ _ _"
-	function underScore (){
-		for (var i = 0; i < word.length; i++){
-		var neWord = word.replace(word[i], "_");
-		document.write(neWord[i]);
-		}
-	};
-
-	//function to find the position of the user input letter
 	function keyIndex(){	
-		for (var i = 0; i < word.length; i++){
-		var index = word.indexOf(pressedKey);
+		for (var i = 0; i < wordGenerator.length; i++){
+		var index = wordGenerator.indexOf(pressedKey);
 		console.log(index);
 		}
 	};
+};
+// 	console.log(pressedKey);
+// 	console.log(wordGenerator);
+// 	underScore();
+// 	keyIndex();
+
+// //function to find the position of the user input letter
+// function keyIndex(){	
+// 	for (var i = 0; i < wordGenerator.length; i++){
+// 	var index = wordGenerator.indexOf(pressedKey);
+// 	console.log(index);
+// 	}
+// };
+		
 
 
 
+//function to convert the word from the pool into "_ _ _ _ _ _ _"
 
-
-	// var isPresent = arr.includes(pressedKey);
-
-	// var index = .indexOf(pressedKey);
-
-	// // console.log(isPresent);
-	// console.log(index);
-	
+function generateUnderScore(){
+	for (var i = 0; i < wordGenerator.length; i++){
+		underScore.push("_");
+	}
+	var targetWord = document.getElementById('wordUnderscore');
+	targetWord.innerHTML = "<p id='underscores'>" + underScore + "</p>";
 };
 
-// function aFunction(){
-// 	var word = wordGenerator;
-// 	for (var i = 0; i < word.length; i++);
-// 		var neWord = word.map(function(x){return x.replace(/word/g, "_ ")});
-// 	console.log(neWord);
 
-// }
+
+
